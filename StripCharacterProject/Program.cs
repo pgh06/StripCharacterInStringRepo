cb3ff8f7004111f99d6fa7721fe7c60ccea0e906
Peter Hansen <phansen@brainshark.com>
2020-08-16 21:43:22 -0400

    Initial commit

---
+++ StripCharacterProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StripCharacterProject
+{
+    class Program
+    {
+        public static string StripZeros(string n)
+        {
+            if (n.StartsWith("0"))
+            {
+                string substring = n.Substring(1);
+                var newString = StripZeros(substring);
+                return newString;
+            }
+
+            return n;
+        }
+
+        static void Main(string[] args)
+        {
+            var test1 = StripZeros("0000000000004354");
+            var test2 = StripZeros("00fd04fdsgfdsgs");
+            var test3 = StripZeros("0000000000004fdgdfgdgd354");
+            var test4 = StripZeros("00000000fdg4354");
+        }
+    }
+}
